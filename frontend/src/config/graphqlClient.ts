9fa77cb50ab1166f9c3d9126af35bccae93dfd00
Drack <61944386+Drack112@users.noreply.github.com>
2022-09-07 14:58:14 -0300

    🐛 FIX: url of api

--- frontend/src/config/graphqlClient.ts
+++ frontend/src/config/graphqlClient.ts
@@ -3,11 +3,11 @@ import { createClient as createWsClient } from "graphql-ws"
 import { createClient, subscriptionExchange, defaultExchanges } from "urql"
 
 const wsClient = createWsClient({
-  url: "ws://localhost:8000/graphql",
+  url: "ws://localhost:4000/graphql",
 })
 
 const graphqlClient = createClient({
-  url: "http://localhost:8000/graphql",
+  url: "http://localhost:4000/graphql",
   exchanges: [
     ...defaultExchanges,
     subscriptionExchange({
