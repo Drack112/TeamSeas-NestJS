d132ebba38f25346e849b9ca486f4ec52052ffdf
Drack <61944386+Drack112@users.noreply.github.com>
2022-09-07 12:58:07 -0300

    ⚡ General Update:  Framer Motion Animation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import * as React from "react"
 import { ChakraProvider, Box, Text, VStack, Grid, Heading, extendTheme } from "@chakra-ui/react"
 
 import { Logo } from "./Logo"
+import { Counter } from "./components/donation/Counter"
 
 const theme = extendTheme({
   fonts: {
@@ -20,8 +21,14 @@ export const App = () => (
       <Grid minH='100vh' p={3}>
         <VStack spacing={8}>
           <Logo h='32' pointerEvents='none' />
-          <Heading>JOIN IN THE MOVEMENT!</Heading>
-          <Text>The team is growing everyday and scoring wins</Text>
+          <Heading>JOIN IN THE MOVEMENT!</Heading>;
+          <Text>
+            The team is growing everyday and scoring wins for the planet.
+            <br /> Remove trash with us and track our progress!
+          </Text>
+          <Heading as='h2' size='4xl'>
+            <Counter from={0} to={42545} />
+          </Heading>
         </VStack>
       </Grid>
     </Box>
