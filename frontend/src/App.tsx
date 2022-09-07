f1712e6fe686090217963afc3ed4c40f05764b33
Drack <61944386+Drack112@users.noreply.github.com>
2022-09-07 14:58:44 -0300

    ⚡ General Update:  Star communication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,12 @@ import "./components/donation/fonts"
 
 import * as React from "react"
 import { ChakraProvider, Box, Text, VStack, Grid, Heading, extendTheme } from "@chakra-ui/react"
+import { useQuery, useSubscription } from "urql"
 
 import { Logo } from "./Logo"
 import { Counter } from "./components/donation/Counter"
+
+import { TotalDonationsQuery, TotalUpdatedQuery } from "./querys"
 
 const theme = extendTheme({
   fonts: {
@@ -13,22 +16,36 @@ const theme = extendTheme({
   },
 })
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign='center' fontSize='xl'>
-      <Grid minH='100vh' p={3}>
-        <VStack spacing={8}>
-          <Logo h='32' pointerEvents='none' />
-          <Heading>JOIN IN THE MOVEMENT!</Heading>;
-          <Text>
-            The team is growing everyday and scoring wins for the planet.
-            <br /> Remove trash with us and track our progress!
-          </Text>
-          <Heading as='h2' size='4xl'>
-            <Counter from={0} to={42545} />
-          </Heading>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+const handleSubscription = (previous: any, newTotal: any) => {
+  return newTotal?.totalUpdated.total
+}
+
+export const App = () => {
+  const [res] = useSubscription({ query: TotalUpdatedQuery }, handleSubscription)
+  const [{ data, fetching, error }] = useQuery({
+    query: TotalDonationsQuery,
+  })
+
+  if (fetching) return <p>Loading</p>
+  if (error) return <p>Oh no... {error.message}</p>
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign='center' fontSize='xl'>
+        <Grid minH='100vh' p={3}>
+          <VStack spacing={8}>
+            <Logo h='32' pointerEvents='none' />
+            <Heading>JOIN IN THE MOVEMENT!</Heading>;
+            <Text>
+              The team is growing everyday and scoring wins for the planet.
+              <br /> Remove trash with us and track our progress!
+            </Text>
+            <Heading as='h2' size='4xl'>
+              <Counter from={0} to={res.data || data.totalDonations} />
+            </Heading>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  )
+}
